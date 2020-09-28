4ce142b6cd602312eff9529595c9d27dd3c1be07
cyproto <kyashkarande@gmail.com>
2020-09-29 00:05:26 +0530

    Prepare model and show status images.

---
+++ lib/models/home_screen_model.dart
@@ -0,0 +1,22 @@
+class HomeScreenModel {
+  static final statusImages = [
+    'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043233-anime-away-face-no-nobody-spirited_113254.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043232-avatar-batman-comics-hero_113278.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043250-avatar-child-girl-kid_113270.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043254-avatar-elderly-grandma-nanny_113280.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043251-avatar-female-girl-woman_113291.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043260-avatar-male-man-portrait_113269.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043241-builder-helmet-worker_113249.png',
+    'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043274-avatar-einstein-professor-scientist_113259.png',
+  ];
+}
+
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043238-avatar-boy-kid-person_113284.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043272-avatar-lazybones-sloth-sluggard_113274.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043255-beard-hipster-male-man_113243.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043252-child-girl-kid-person_113255.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043240-avatar-bad-breaking-chemisrty-heisenberg_113279.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043239-baby-child-kid-toddler_113286.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043248-avatar-female-portrait-woman_113285.png"/>
+// <img src="https://cdn.icon-icons.com/icons2/1736/PNG/128/4043270-avatar-joker-squad-suicide-woman_113256.png"/>
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
 
+import '../models/home_screen_model.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         color: Color(0xff128c7e),
+        child: Container(
+          height: 350,
+          child: Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(left: 40, top: 70),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Chat app',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width - 40,
+                      height: 70,
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          if (index < HomeScreenModel.statusImages.length) {
+                            return Container(
+                              margin: EdgeInsets.symmetric(horizontal: 3),
+                              child: CircleAvatar(
+                                radius: 35,
+                                backgroundImage: NetworkImage(
+                                    HomeScreenModel.statusImages[index]),
+                                backgroundColor: Colors.transparent,
+                              ),
+                            );
+                          }
+                        },
+                        scrollDirection: Axis.horizontal,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
