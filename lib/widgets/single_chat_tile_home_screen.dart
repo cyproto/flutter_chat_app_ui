c7fa143a16031938c99a4610ef1c6e369af702b5
cyproto <kyashkarande@gmail.com>
2020-09-29 23:55:16 +0530

    lib/widgets/single_chat_tile_home_screen.dart: Initial commit.

---
+++ lib/widgets/single_chat_tile_home_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SingleChatTileHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
