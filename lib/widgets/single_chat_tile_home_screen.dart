dd8232bad7259689b6e63750937706bfa8c10471
cyproto <kyashkarande@gmail.com>
2020-10-01 00:26:50 +0530

    lib/widgets/single_chat_tile_home_screen.dart: Add unseen message count.

--- lib/widgets/single_chat_tile_home_screen.dart
+++ lib/widgets/single_chat_tile_home_screen.dart
@@ -44,17 +44,39 @@ class SingleChatTileHomeScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: Row(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
-                  child: CircleAvatar(
-                    radius: 35,
-                    backgroundImage: NetworkImage(profileImageUrl),
-                    backgroundColor: Colors.transparent,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+                      child: CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(profileImageUrl),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    if (unreadMessageCount != 0)
+                      Positioned(
+                        top: 5,
+                        left: 5,
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.orangeAccent,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Text(
+                            unreadMessageCount.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                  ],
                 ),
                 Container(
                   margin: EdgeInsets.only(left: 20),
